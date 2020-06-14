8c490dbfb9b26c63ef53c451e579dcfd66fc2097
tom <tom@sapletta.com>
2020-06-14 13:52:55 +0200

    first version

--- remote_update.bat
+++ remote_update.bat
@@ -1,5 +1,5 @@
 cd F:\PycharmProjects\faas-ovh-api
 py deploy.py www.faas.ovh remove.sh
-py deploy.py www.faas.ovh install.sh
+::py deploy.py www.faas.ovh install.sh
 ::py deploy.py www.faas.ovh update.sh
 cd F:\WebstormProjects\faas-ovh-www
